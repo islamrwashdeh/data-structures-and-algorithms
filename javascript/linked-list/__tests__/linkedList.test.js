5eb7c829435e6fb48710928a60f59ed7f69ca2c6
sal14ma <islamrwashdeh@yahoo.com>
2022-06-15 16:10:19 +0300

    add kth from end

--- javascript/linked-list/__tests__/linkedList.test.js
+++ javascript/linked-list/__tests__/linkedList.test.js
@@ -2,37 +2,71 @@
 
 const LinkedList = require('../lib/linkedlist');
 
-describe("link list", () => {
-    test("create a linklist", () => {
-        let list = new LinkedList();
-        expect(list.head).toBeNull();
-    })
-    test("append to linklist works", () => {
-        let list = new LinkedList();
-        list.append('one');
-        expect(list.head.value).toEqual('one');
-        list.append('two');
-        expect(list.head.value).toEqual('one');
-        list.append(4);
-        list.append(5);
-        list.append(7);
-        list.append('8');
-        list.append('9');
-        list.append(10);
-        expect(list.head.value).toEqual('one');
-    })
-});
 
-////test  insert 
-describe('insert to the beginning of linklst ', () => {
   
-  it('test case 1 add to empty Linklst', () => {
-    const ll = new LinkedList();
-    ll.insert('a');
-    expect(ll.head.value).toBe('a');
-    expect(ll.head.next).toBeNull();
-  })
+// ////test  insert np
+// describe('insert to the beginning of linklst ', () => {
+  
+//   it('test case 1 add to empty Linklst', () => {
+//     const ll = new LinkedList();
+//     ll.insert('a');
+//     expect(ll.head.value).toBe('a');
+//     expect(ll.head.next).toBeNull();
+//   })
+// });
+
+
+
+describe('Linked List', () => {
+ 
+  it('test insert before ',()=>{
+    const link = new LinkedList();
+    link.append('A');
+    link.append('B');
+    link.append('C');
+  
+    link.insertBefore('C','D');
+    console.log(link.toString());
+    expect(link.head.value).toBe('A');
+    expect(link.head.next.value).toBe('B');
+    expect(link.head.next.next.value).toBe('D');
+    expect(link.head.next.next.next.value).toBe('C');
+    expect(link.head.next.next.next.next).toBe(null);
+  });  
+
+
+  it('test insert after',()=>{
+    const link = new LinkedList();
+    link.append('A');
+    link.append('B');
+    link.append('C');
+  
+    link.insertAfter('B','E');
+    console.log(link.toString());
+    expect(link.head.value).toBe('A');
+ 
+    expect(link.head.next.value).toBe('B');
+    expect(link.head.next.next.value).toBe('E');
+    expect(link.head.next.next.next.value).toBe('C');
+    expect(link.head.next.next.next.next).toBe(null);
+  });
 });
 
+
+
+////////// kthFromEnd//////////
+
+describe('test kthFromEnd', () => {
+  it('test kthFromEnd cases', () => {
+    const ll = new LinkedList();
+    ll.insert(0);
+    ll.insert(2);
+    ll.insert(6);
+    expect(ll.kthFromEnd(6)).toEqual('Exception');
+    expect(ll.kthFromEnd(2)).toEqual(3);
+    expect(ll.kthFromEnd(0)).toEqual(2);
+   
+  });
+});
 
 
--- javascript/linked-list/lib/linkedlist.js
+++ javascript/linked-list/lib/linkedlist.js
@@ -1,112 +1,137 @@
+
+
 'use strict';
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-        // length 
-        // tail
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+      //insert 
+  insert(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
+  }
 
-    //append 
-    append(value) {
-        const newNode = new Node(value);
-        //we need to check if the LL is empty or not 
-        
-        if (!this.head) {
-            this.head = newNode;
-            return this;
-        }
-        else {
-            // if the LL is not empty
-            let currentNode = this.head;
-            while (currentNode.next) {
-                currentNode = currentNode.next;
-            }
-            currentNode.next = newNode;
-            return this;
-        }
+
+
+// include  
+
+include(value){
+  if(this.head!=null){
+      let includehead = this.head;
+      while(includehead!=null){
+    
+    if(includehead.value!=value){
+
+    includehead =includehead.next;
+}
+else return true;
+}
+
+}
+else return false;
+}
+
+
+//////////////class 6/////////
+  append(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+      this.length++;
     }
+  }
 
-                    //Code Challenge: Class 06//
-
-
-     
-    insertBefore(previousValue, insertedValue) {
-        const node = new Node(insertedValue);
-        let current = this.head;
-        while (current.next != null) {
-            current = current.next;
-            if (current.next.value == previousValue) {
-                this.length++;
-                node.next = current.next;
-                current.next = node;
-                return null;
-            }
-        }
-
+  insertBefore(perValue, newVal) {
+    const node = new Node(newVal);
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      if (currentNode.next.value == perValue) {
+        node.next = currentNode.next;
+        currentNode.next = node;
+        return null;
+      }
     }
-    insertAfter(previousValue, insertedValue) {
-        const node = new Node(insertedValue);
-        let current = this.head;
-        while (current.next != null) {
-            current = current.next;
-            if (current.value == previousValue) {
-                this.length++;
-                node.next = current.next;
-                current.next = node;
-                return null;
-            }
-        }
+  }
 
-
+  insertAfter(perValue, newVal) {
+    const node = new Node(newVal);
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      if (currentNode.value == perValue) {
+        node.next = currentNode.next;
+        currentNode.next = node;
+        return null;
+      }
     }
-
-
-
-
-
-// ************************************************//
-
+  }
+///////////// kth from end////////
  
-        //   //insert 
-        //   insert(value){
-        //     const newNode = new Node(value);
-        //       if(!this.head){
-        //         this.head = newNode;
-        //       } else{
-        //           newNode.next = this.head;
-        //           this.head = Node;
-        //       }
+kthFromEnd(k) {
+  let current = this.head;
+  let length = 0;
+  while (current) {
+    current = current.next;
+    length++;
+  }
+  if (k < 0 || k > length) {
+    return "exception";
+  } else {
+    length = length - 1 - k;
+    current = this.head;
+    while (length > 0) {
+      current = current.next;
+      length--;
+    }
+    return current.value;
+  }
+}
+          
 
-        //   }
 
-
-        //   // include  
-
-        //   include(value){
-        //       if(this.head!=null){
-        //           let includehead = this.head;
-        //           while(includehead!=null){
-                
-        //         if(includehead.value!=value){
-
-        //         includehead =includehead.next;
-        //     }
-        //     else return true;
-        //     }
-       
-        //   }
-        //   else return false;
-        //   }
-
-//*********************************************************** */
          
           
 
 
+  
 
+
+  
+
+  
 }
-
-
 module.exports = LinkedList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
