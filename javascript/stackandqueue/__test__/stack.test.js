384146d72b5a0f30224d6d0e24b3b92dbc4f2a5b
sal14ma <islamrwashdeh@yahoo.com>
2022-06-21 16:52:27 +0300

    test

---
+++ javascript/stackandqueue/__test__/stack.test.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const Stack = require("../stack/lib/Stack");
+
+describe("testing the Stack", () => {
+  it("Can successfully instantiate an empty stack", () => {
+    let newStack = new Stack();
+    expect(newStack instanceof Stack).toBeTruthy();
+  });
+
+  it("Can successfully peek the next item on the stack", () => {
+    let newStack = new Stack();
+    expect(newStack.peek()).toBeNull();
+  });
+
+  it("Can successfully push onto a stack", () => {
+    let newStack = new Stack();
+    newStack.push(1);
+    newStack.push(2);
+    expect(newStack.top.value).toEqual(2);
+    expect(newStack.top.next.value).toEqual(1);
+  });
+
+  it("Can successfully pop off the stack", () => {
+    let newStack = new Stack();
+    newStack.push(1);
+    newStack.push(2);
+    newStack.push(3);
+    newStack.push(4);
+    expect(newStack.pop()).toEqual(4);
+    expect(newStack.pop()).toEqual(3);
+    expect(newStack.pop()).toEqual(2);
+  });
+
+  it("Can successfully peek the next item on the stack", () => {
+    const newStack = new Stack();
+    const newStack2 = new Stack();
+    newStack.push(3);
+    newStack.push(4);
+
+    expect(newStack.isEmpty()).toBeFalsy();
+    expect(newStack2.isEmpty()).toBeTruthy();
+  });
+});
---
+++ javascript/stackandqueue/stack/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Stack = require('./lib/Stack');
+
+const newStack = new Stack();
+
+newStack.push(1);
+newStack.push(2);
+newStack.push(3);
+console.log(newStack.peek());//3
+newStack.pop();
+newStack.pop();
+console.log(newStack.peek());//1
+newStack.pop();
+newStack.pop();
+console.log(newStack.peek());//
